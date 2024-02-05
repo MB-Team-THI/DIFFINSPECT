fe4a55b66401cd4a3761ff612d3cf6399f90d7dc
Fertig <alexander.fertig@thi.de>
2024-02-05 16:08:05 +0100

    Updates

---
+++ precache-manifest.143a1c74a2bbb8c5e6f8feaa89a72525.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "609df7fd3f8677c72b95",
+    "url": "/DIFFINSPECT/static/css/main.fdf31900.chunk.css"
+  },
+  {
+    "revision": "609df7fd3f8677c72b95",
+    "url": "/DIFFINSPECT/static/js/main.609df7fd.chunk.js"
+  },
+  {
+    "revision": "caaf9aa8dcd06c7ab6d0",
+    "url": "/DIFFINSPECT/static/js/1.caaf9aa8.chunk.js"
+  },
+  {
+    "revision": "07ca783f448d63677e7c",
+    "url": "/DIFFINSPECT/static/js/runtime~main.07ca783f.js"
+  },
+  {
+    "revision": "fedb3279c7590096cfdef55e3cfad5c8",
+    "url": "/DIFFINSPECT/index.html"
+  }
+];
